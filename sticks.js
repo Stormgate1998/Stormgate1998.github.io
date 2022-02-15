9ee61bf724425637a5086fdcce3ce3a9b92c99c9
Joshua Barlow <jalapenojosh@gmail.com>
2022-02-15 12:07:35 -0700

    ds

--- sticks.js
+++ sticks.js
@@ -6,13 +6,13 @@ var sticks = prompt("What is the number of sticks?");
 while (sticks > 0) {
     p1 = 4;
     p2 = 4;
-    while (p1 > 3 || p1 <= 0 || p1 == "Player 1, choose a number between 1 and 3") {
+    while (p1 > 3 || p1 <= 0 || p1 === "Player 1, choose a number between 1 and 3") {
         p1 = prompt(sticks, "Player 1, choose a number between 1 and 3");
     }
     sticks = sticks - p1;
     player = false;
     if (sticks > 0) {
-        while (p2 > 3 || p2 <= 0 || p2 == "Player 2, choose a number between 1 and 3") {
+        while (p2 > 3 || p2 <= 0 || p2 === "Player 2, choose a number between 1 and 3") {
             p2 = prompt(sticks, "Player 2, choose a number between 1 and 3");
         }
         sticks = sticks - p2;
