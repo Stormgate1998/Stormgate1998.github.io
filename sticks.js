2c8de033d1c9f167812b4b8d19645b07b5d1bbef
Stormgate1998 <jalapenojosh@gmail.com>
2022-02-19 22:18:23 -0700

    restored to previous working version

--- sticks.js
+++ sticks.js
@@ -1,43 +1,35 @@
-function getInput(p1, player) {
+var player = false;
+var sticks = prompt("What is the number of sticks you want to play with?");
+while (sticks > 0) {
+    let p1 = 4;
+    let p2 = 4;
     let wronginfo = false;
     let userinput = false;
     while (!userinput) {
         if (wronginfo) {
             alert("This is an invalid input. Please choose a number between 1 and 3");
         }
-        if (!player) {
-            p1 = (`${sticks} Player 1 choose a number between 1 and 3`);
-        } else {//changes from player 1 to player 2
-            p1 = (`${sticks} Player 2, choose a number between 1 and 3`);
-        }
-        
+        p1 = prompt(`${sticks} Player 1, choose a number between 1 and 3`);
         if (p1 <= 3 && p1 > 0) {
             wronginfo = true;
             userinput = !userinput;
-
         }
     }
-    return p1;
-}
-var player = false;
-var sticks = prompt("What is the number of sticks you want to play with?");
-while (sticks > 0) {
-    let p1 = 4;
-    let p2 = 4;
-    p1 = getInput(p1, player);
+    wronginfo = false;
     sticks = sticks - p1;
-    player = true;
-
+    player = false;
+    userinput = false;
     if (sticks > 0) {
-        p2 = getInput(p2, player);
-        sticks = sticks - p2;
-        player = false;
-    }
-}
-if (player) {
-    alert("Player 1 wins!");
-} else {
-    alert("Player 2 wins!");
-}
-//put it into the html file
-//extract stuff to methods to remove repeating
+        while (!userinput) {
+            if (wronginfo) {
+                alert("This is an invalid input. Please choose a number between 1 and 3");
+            }
+            p2 = prompt(`${sticks} Player 2, choose a number between 1 and 3`);
+            if (p1 <= 3 && p1 > 0) {
+                wronginfo = true;
+                userinput = !userinput;
+                @@ -45, 4 + 45, 6 @@ if (player) {
+                    alert("Player 1 wins!");
+                } else {
+                    alert("Player 2 wins!");
+                }
