0bbb347918055405d460f6ce67a7fbf54cfa99a5
Stormgate1998 <jalapenojosh@gmail.com>
2022-04-23 16:30:17 -0600

    asdfgvb

--- finalproject/dndcode.js
+++ finalproject/dndcode.js
@@ -34,8 +34,38 @@ function Combatinfo() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
+    if (!event.target.matches('.char')) {
+        var dropdowns = document.getElementsByClassName("character");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+    if (!event.target.matches('.com')) {
+        var dropdowns = document.getElementsByClassName("combat");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+    if (!event.target.matches('.mag')) {
+        var dropdowns = document.getElementsByClassName("magIc");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+    if (!event.target.matches('.gen')) {
+        var dropdowns = document.getElementsByClassName("general");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
