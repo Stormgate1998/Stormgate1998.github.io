3a3835127a724eb1558edd64b9a808dd1efdf95e
Stormgate1998 <jalapenojosh@gmail.com>
2022-04-23 16:06:39 -0600

    fwerh

---
+++ finalproject/dndcode.js
@@ -0,0 +1,118 @@
+function menubar(url) {
+    wipePage(document.getElementById('main'));
+    goFetch(url);
+}
+/*
+ * To make it work:
+ * get names of all elements
+ * get values of all elements
+ * FIGURE OUT IF IT'S AN OBJECT
+ * IF SO, BREAK IT IN PIECES
+ * display all as elements
+ * 
+ * 
+ * 
+ * Take each of the two functions
+ * Make 4 of each
+ * customize accordingly
+ * 
+ */
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function Charinfo() {
+    document.getElementById("charInfo").classList.toggle("show");
+}
+function Combatinfo() {
+    document.getElementById("combatInfo").classList.toggle("show");
+} function Maginfo() {
+    document.getElementById("magInfo").classList.toggle("show");
+} function Geninfo() {
+    document.getElementById("genInfo").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+window.addEventListener("load", function () {
+    // const myElement = document.querySelector("body");
+    // goFetch("/api/monsters/mimic"); 
+
+});
+
+//Takes an object, presents a list of all items
+function getList(sheet, addition = "") {
+    const myElement = document.getElementById("main");
+    //wipePage(myElement);
+    var names = Object.values(sheet);
+    var properties = Object.keys(sheet);
+    let index = 0;
+    names.forEach(element => {
+        if ((typeof element) == "object") {
+            //check API to view discrepencies
+            let thing = document.createElement("h3");
+            if (properties[index] != 0) {
+                thing.innerText = properties[index];
+            }
+            myElement.appendChild(thing);
+            getList(element/*, (addition + "       ")*/);
+        } else {
+            let item;
+            let item2;
+            element = String(element);
+            if (element.includes("/")) {
+                item = document.createElement("p");
+                item2 = document.createElement("a");
+                item2.innerText = "To learn more, click here";
+                item2.addEventListener("click", (event) => {
+                    wipePage(document.getElementById("main"));
+                    console.log(element);
+                    goFetch(element);
+                }
+
+                );
+                item.appendChild(item2);
+            } else {
+                item = document.createElement("p");
+                item.innerText = addition + properties[index] + ': ' + element;
+            }
+            myElement.appendChild(item);
+
+            index++;
+        }
+    })
+
+}
+//takes a piece of url, fetches list. If an item is an object, loop. If value is empty, print N/A
+function goFetch(url) {
+    let source = "https://www.dnd5eapi.co" + url;
+
+    fetch(source)
+        .then((result) => result.json())
+        .then((sheet) => {
+            getList(sheet);
+        });
+}
+//
+//given an element, systematically removes all children from it
+function wipePage(subject) {
+    while (subject.firstChild) {
+        subject.removeChild(subject.lastChild);
+    }
+}
+//takes any string and checks for appropriate uppercase. Removes _ as well
+function modInput(stringIn) {
+
+}
