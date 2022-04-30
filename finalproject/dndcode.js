52559c766545aa9744780004682faf95d05d977b
Stormgate1998 <jalapenojosh@gmail.com>
2022-04-30 13:13:49 -0600

    reverted code

--- finalproject/dndcode.js
+++ finalproject/dndcode.js
@@ -3,6 +3,8 @@ function menubar(url) {
     goFetch(url);
 }
 var currenturl;
+
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function Charinfo() {
@@ -19,26 +21,89 @@ function Combatinfo() {
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches('.char')) {
-        window.onclick = function (event) {
+        var dropdowns = document.getElementsByClassName("character");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-        let h3limit;
-        //Takes an object, presents a list of all items
-        function getList(sheet, addition = "") {
-            const myElement = document.getElementById("main");
-            //wipePage(myElement);
-            var names = Object.values(sheet);
-            var properties = Object.keys(sheet);
-            function getList(sheet, addition = "") {
-                if (properties[index] != 0 && properties[index] != h3limit) {
-                    thing.innerText = properties[index];
-                    h3limit = properties[index];
-                }
-                myElement.appendChild(thing);
-                getList(element/*, (addition + "       ")*/);
+    }
+    if (!event.target.matches('.com')) {
+        var dropdowns = document.getElementsByClassName("combat");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+    if (!event.target.matches('.mag')) {
+        var dropdowns = document.getElementsByClassName("magIc");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+    if (!event.target.matches('.gen')) {
+        var dropdowns = document.getElementsByClassName("general");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+let h3limit;
+//Takes an object, presents a list of all items
+function getList(sheet, addition = "") {
+    const myElement = document.getElementById("main");
+    //wipePage(myElement);
+    var names = Object.values(sheet);
+    var properties = Object.keys(sheet);
+    let index = 0;
+    names.forEach(element => {
+        if ((typeof element) == "object") {
+            //check API to view discrepencies
+            let thing = document.createElement("h3");
+            if (properties[index] != 0 && properties[index] != h3limit) {
+                thing.innerText = properties[index];
+                h3limit = properties[index];
+            }
+            myElement.appendChild(thing);
+            getList(element/*, (addition + "       ")*/);
+        } else {
+            let item;
+            let item2;
+            element = String(element);
+            if (properties[index] === "count" || properties[index] === "index" || properties[index] === "_id" || element === currenturl) {
+
             } else {
-                let item;
-                let item2;
-               function getList(sheet, addition = "") {
+
+            
+            if (element.includes("/")) {
+                item = document.createElement("p");
+                item2 = document.createElement("a");
+                item2.innerText = "To learn more, click here";
+                item2.addEventListener("click", (event) => {
+                    wipePage(document.getElementById("main"));
+                    console.log(element);
+                    goFetch(element);
+                }
+
+                );
+                item.appendChild(item2);
+            } else {
+                item = document.createElement("p");
+                let stringInput;
+                if (properties[index] == 0) {
                     stringInput = element;
                     stringInput = stringInput;
                 } else {
@@ -46,15 +111,58 @@ window.onclick = function (event) {
                     stringInput = stringInput;
                 }
                 item.innerText = stringInput;
-                function modInput(stringIn) {
-                }
-                    function arrayList(subject) {
-                        let container;
-                        const myElement = document.getElementById("main");
-                        subject.forEach(function (currval) {
-                            container = document.createElement("div");
-                            for (let props in currval) {
+            }
+            myElement.appendChild(item);
+            }
+            index++;
+        }
+    })
 
-                            }
-                        });
-                    }
+}
+//takes a piece of url, fetches list. If an item is an object, loop. If value is empty, print N/A
+function goFetch(url) {
+    let source = "https://www.dnd5eapi.co" + url;
+    currenturl = url;
+    fetch(source)
+        .then((result) => result.json())
+        .then((sheet) => {
+            getList(sheet);
+        });
+}
+//
+//given an element, systematically removes all children from it
+function wipePage(subject) {
+    while (subject.firstChild) {
+        subject.removeChild(subject.lastChild);
+    }
+}
+//takes any string and checks for appropriate uppercase. Removes _ as well
+function modInput(stringIn) {
+    stringIn = String(stringIn);
+    stringIn = stringIn.replace("-", " ");
+    stringIn = stringIn.replace("_", " ");
+    stringIn.charAt(0).toUpperCase();
+// if string contains - or _, replace with space.
+    //if string doesn't start with capital letter, make captialized
+}
+
+function arrayList(subject) {
+    let container;
+    subject.forEach(function () {
+        container = document.createElement("div");
+
+    });
+}
+/* TO DO list:
+ * Figure out how to block things into appropriate divs
+ * do so
+ * in html/css add grid format to make reading easier
+ * dynamically change formatting
+ * 
+ * build and implement modInupt
+ * alter display to hide unwanted elements
+ * 
+ * Make everything look nice
+ *
+ * prevent repeating elements
+ */
